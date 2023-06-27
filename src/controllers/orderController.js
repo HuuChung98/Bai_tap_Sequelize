ecb14660002b884846688f8b9c2661ec404d045a
Odin98 <balechung93@gmail.com>
2023-06-27 22:38:44 +0700

    update API

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,15 +1,16 @@
 import initModels from "../models/init-models.js";
 import sequelize from "../models/index.js";
+import { successCode, failCode, errorCode } from "../config/response.js";
 
 let models = initModels(sequelize);
 
-const processOrder = (req, res) => {
+const processOrder = async (req, res) => {
   try {
     let { user_id, food_id, amount } = req.body;
-    models.order.create({ user_id, food_id, amount });
-    res.send("them mon thanh cong");
+     await models.order.create({ user_id, food_id, amount });
+     successCode(res, "them mon thanh cong");
   } catch {
-    res.send("them mon that bai");
+    failCode(res, "them mon that bai");
   }
 };
 
