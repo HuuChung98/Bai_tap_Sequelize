8f5573f35905fa85282af284d6cc979b80c142a6
Odin98 <balechung93@gmail.com>
2023-06-27 22:32:53 +0700

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,26 +4,38 @@ import { successCode, failCode, errorCode } from "../config/response.js";
 
 let models = initModels(sequelize);
 
+// const processLike = async (req, res) => {
+//   try {
+//     let { user_id } = req.params;
+//     let listLikeRes = await models.like_res.findOne({
+//       include: ["re"],
+//     },{
+//       where: { user_id }
+//     });
+//     successCode(res, listLikeRes, "lấy data thành công");
+//   } catch {
+//     failCode(res ,"lấy data thất bại");
+//   }
+// };
+
 const processLike = async (req, res) => {
   try {
-    let { user_id } = req.params;
-    let listLikeRes = await models.like_res.findAll({
-      include: ["re"],
-    },{
-      where: { user_id }  
+    let { user_id  } = req.params;
+    let listRes = await models.like_res.findAll( {
+      where: {user_id}
     });
-    successCode(res, listLikeRes, "lấy data thành công");
+    successCode(res, listRes, "lấy data thành công");
   } catch {
-    failCode(res ,"lấy data thất bại");
+    failCode(res, "lấy data thất bại");
   }
 };
 
 const processRate = async (req, res) => {
   try {
-    // let { user_id, like_res } = req.params;
+    let { user_id } = req.params;
 
     let listRateRes = await models.rate_res.findAll({
-      include: ["re"],
+      where: {user_id}
     });
     successCode(res, listRateRes, "lấy data thất thành công");
   } catch {
